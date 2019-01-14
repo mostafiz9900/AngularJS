52715402377f71c15c5041aabf3131337634f5ab
Your Name <you@example.com>
2019-01-15 01:16:54 +0600

    menu

---
+++ chapter_2/web/cont.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+app.directive("testDirecTest", function() {
+    return {
+        template :"Mostafizur Rahman is a java programmer"
+    };
+});
